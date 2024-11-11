d145f4e53b45410d312debd110868bbda8c06b8b
y-nagayoshi <wakasagi135@gmail.com>
2024-11-11 23:52:24 +0900

    CMS機能実装中

--- educa/urls.py
+++ educa/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('course/', include('courses.urls')),
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update'
+    ),
 ]
 
 
